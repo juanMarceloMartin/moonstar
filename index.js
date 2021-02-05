da039439bba9885f737816e213073583dd4aa1ca
Marce <jmarce_83@hotmail.com>
2021-02-05 18:28:55 -0300

    bug fixed

--- index.js
+++ index.js
@@ -15,138 +15,135 @@ const commentDate = document.querySelector('.comment-date');
 
 const date = new Date();
 
-function addCommentsToPosts() {
-    fetch('https://jsonplaceholder.typicode.com/comments')
-        .then(response => response.json())
-        .then(comments => {
-            for (let i = 0; i < posts.length; i++) {
-                posts[i]['comments'] = [];
-                for (let j = 0; j < comments.length; j++) {
-                    if (posts[i].id === comments[j].postId) {
-                        posts[i]['comments'].push(comments[j]);
-                     }
-                }
+const methods = {
+    pushPostToPostsArray: retrievedPosts => {
+        for (let i = 0; i < 10; i++) {
+            posts.push(retrievedPosts[i]);
+            const card = document.createElement('div')
+            card.setAttribute('id', `card-${i}`);
+            card.setAttribute('class', 'polaroid');
+            container.append(card)
+
+            function fillArticle() {
+                clickedPostId = i;
+                container.classList.add('d-none');
+                articleContainer.classList.remove('d-none');
+                articleTitle.innerText = posts[i].title;
+                articleImage.setAttribute('src', posts[i].urls.regular);
+                articleBody.innerText = posts[i].body;
+
+                methods.displayComments(posts[i].comments);
+
+                window.scrollTo(0, 0);
             }
-        })
-}
 
-function addImagesToPosts() {
-    fetch('https://api.unsplash.com/search/photos?query=beach&page=1&client_id=z5tR6xI7D8CCROM12n5E0358ttG5G2dYLc1G-HEOUpE')
-        .then(response => response.json())
-        .then(images => {
-            for (let i = 0; i < posts.length; i++) {
-                posts[i]['urls'] = images.results[i]['urls'];
-                const picture = document.createElement('img');
-                picture.setAttribute('src', images.results[i]['urls'].small);
-                picture.setAttribute('class', 'card-image');
-                document.querySelector(`#card-${i}`).append(picture);
-                const title = document.createElement('h4')
-                document.querySelector(`#card-${i}`).append(title);
-                title.innerText = posts[i].title;
+            card.addEventListener('click', fillArticle)
+        }
+    },
+    createCommentsArray: comments => {
+        for (let i = 0; i < posts.length; i++) {
+            posts[i]['comments'] = [];
+            for (let j = 0; j < comments.length; j++) {
+                if (posts[i].id === comments[j].postId) {
+                    posts[i]['comments'].push(comments[j]);
+                 }
             }
-        })
-}
+        }
+    },
+    addCommentsToPostObject: () => {
+        fetch('https://jsonplaceholder.typicode.com/comments')
+            .then(response => response.json())
+            .then(comments => methods.createCommentsArray(comments))
+    },
+    createImageCard: images => {
+        for (let i = 0; i < posts.length; i++) {
+            posts[i]['urls'] = images.results[i]['urls'];
+            const picture = document.createElement('img');
+            picture.setAttribute('src', images.results[i]['urls'].small);
+            picture.setAttribute('class', 'card-image');
+            document.querySelector(`#card-${i}`).append(picture);
+            const title = document.createElement('h4')
+            document.querySelector(`#card-${i}`).append(title);
+            title.innerText = posts[i].title;
+        }
+    },
+    addImagesToPostObject: () => {
+        fetch('https://api.unsplash.com/search/photos?query=beach&page=1&client_id=z5tR6xI7D8CCROM12n5E0358ttG5G2dYLc1G-HEOUpE')
+            .then(response => response.json())
+            .then(images => methods.createImageCard(images))
+    },
+    setPosts: () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(retrievedPosts => methods.pushPostToPostsArray(retrievedPosts))
+            .then(() => methods.addCommentsToPostObject())
+            .then(() => methods.addImagesToPostObject())
+    },
+    displayComments: comments => {
+        for (let i = 0; i < comments.length; i++) {
+            const author = document.createElement('div');
+            const commentDate = document.createElement('div');
+            const commentBody = document.createElement('div');
+    
+            author.setAttribute('class', 'comment-author');
+            commentDate.setAttribute('class', 'comment-date')
+            commentBody.setAttribute('class', 'comment-body');
+    
+            author.innerText = comments[i].name;
+            commentDate.innerText = date.toDateString();
+            commentBody.innerText = comments[i].body;
+    
+            commentsContainer.append(author);
+            commentsContainer.append(commentDate);
+            commentsContainer.append(commentBody);
+        };
+    },
+    postNewComment: () => {
+        const author = document.getElementById('author');
+        const comment = document.querySelector('.comment-box');
 
-
-
-function setPosts() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(res => { 
-                for (let i = 0; i < 10; i++) {
-                    posts.push(res[i]);
-                    const card = document.createElement('div')
-                    card.setAttribute('id', `card-${i}`);
-                    card.setAttribute('class', 'polaroid');
-                    container.append(card)
-
-                    function fillArticle() {
-                        clickedPostId = i;
-                        container.classList.add('d-none');
-                        articleContainer.classList.remove('d-none');
-                        articleTitle.innerText = posts[i].title;
-                        articleImage.setAttribute('src', posts[i].urls.regular);
-                        articleBody.innerText = posts[i].body;
-
-                        displayComments(posts[i].comments);
-
-                        window.scrollTo(0, 0);
-                    }
-
-                    card.addEventListener('click', fillArticle)
-                }
-            }
-        )
-        .then(() => addCommentsToPosts())
-        .then(() => addImagesToPosts())
-}
-
-function displayComments(comments) {
-    for (let i = 0; i < comments.length; i++) {
-        const author = document.createElement('div');
+        const commentAuthor = document.createElement('div');
         const commentDate = document.createElement('div');
-        const commentBody = document.createElement('div');
+        const authorComment = document.createElement('div');
 
-        author.setAttribute('class', 'comment-author');
+        commentAuthor.setAttribute('class', 'comment-author');
         commentDate.setAttribute('class', 'comment-date')
-        commentBody.setAttribute('class', 'comment-body');
+        authorComment.setAttribute('class', 'comment-body');
 
-        author.innerText = comments[i].name;
+        author.value ? commentAuthor.innerText = author.value : commentAuthor.innerText = "Unknown";
         commentDate.innerText = date.toDateString();
-        commentBody.innerText = comments[i].body;
+        authorComment.innerText = comment.value;
 
-        commentsContainer.append(author);
+        commentsContainer.append(commentAuthor);
         commentsContainer.append(commentDate);
-        commentsContainer.append(commentBody);
-    };
-};
+        commentsContainer.append(authorComment);
 
-function postComment() {
-    const author = document.getElementById('author');
-    const comment = document.querySelector('.comment-box');
+        const newComment = {
+            postId: clickedPostId,
+            id: posts[clickedPostId].comments[posts[clickedPostId].comments.length - 1].id + 1,
+            name: author.value,
+            body: comment.value,
+            email: `${name}@mail.com`
+        };
 
-    const commentAuthor = document.createElement('div');
-    const commentDate = document.createElement('div');
-    const authorComment = document.createElement('div');
+        posts[clickedPostId].comments.push(newComment);
 
-    commentAuthor.setAttribute('class', 'comment-author');
-    commentDate.setAttribute('class', 'comment-date')
-    authorComment.setAttribute('class', 'comment-body');
-
-    author.value ? commentAuthor.innerText = author.value : commentAuthor.innerText = "Unknown";
-    commentDate.innerText = date.toDateString();
-    authorComment.innerText = comment.value;
-
-    commentsContainer.append(commentAuthor);
-    commentsContainer.append(commentDate);
-    commentsContainer.append(authorComment);
-
-    const newComment = {
-        postId: clickedPostId,
-        id: posts[clickedPostId].comments[posts[clickedPostId].comments.length - 1].id + 1,
-        name: author.value,
-        body: comment.value,
-        email: `${name}@mail.com`
-    };
-
-    posts[clickedPostId].comments.push(newComment);
-
-    author.value = '';
-    comment.value = '';
+        author.value = '';
+        comment.value = '';
+    },
+    goToHomePage: () => {
+        container.classList.remove('d-none');
+        articleContainer.classList.add('d-none');
+        commentsContainer.innerHTML = '';
+        window.scrollTo(0, 0);
+    }
 }
 
+document.querySelector('.logo').addEventListener('click', methods.goToHomePage);
 
-function goToHomePage() {
-    container.classList.remove('d-none');
-    articleContainer.classList.add('d-none');
-    window.scrollTo(0, 0);
-};
+postCommentInput.addEventListener('click', methods.postNewComment);
 
-document.querySelector('.logo').addEventListener('click', goToHomePage);
-
-postCommentInput.addEventListener('click', postComment);
-
-setPosts();
+methods.setPosts();
 
 
 
